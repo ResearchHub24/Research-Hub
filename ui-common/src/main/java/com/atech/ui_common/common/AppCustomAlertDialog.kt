5d344da16c53d16861d9cc045795ac603567e2f6
aiyu-ayaan <aiyuayaan22@gmail.com>
2024-05-21 23:06:42 +0530

    - Alert dialog is added !

---
+++ ui-common/src/main/java/com/atech/ui_common/common/AppCustomAlertDialog.kt
@@ -0,0 +1,82 @@
+package com.atech.ui_common.common
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.PermIdentity
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.window.DialogProperties
+import com.atech.ui_common.theme.ResearchHubTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppCustomAlertDialog(
+    modifier: Modifier = Modifier
+) {
+    BasicAlertDialog(
+        onDismissRequest = { },
+        modifier = modifier,
+        properties = DialogProperties()
+    ) { }
+}
+
+@Composable
+fun AppAlertDialog(
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+    onDismissRequest: () -> Unit = {},
+    onConfirmation: () -> Unit = {},
+) {
+    AlertDialog(
+        icon = {
+            Icon(icon, contentDescription = "Example Icon")
+        },
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            Text(text = dialogText)
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text("Dismiss")
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CustomDialogPreview() {
+    ResearchHubTheme {
+        AppAlertDialog(
+            dialogTitle = "Confirm",
+            dialogText = "Sure",
+            icon = Icons.Outlined.PermIdentity
+        )
+    }
+}
