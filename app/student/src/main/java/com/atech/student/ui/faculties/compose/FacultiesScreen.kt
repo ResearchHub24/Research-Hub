af1721064900b328ce3a720c671349fbf597255a
aiyu-ayaan <aiyuayaan22@gmail.com>
2024-05-29 21:10:07 +0530

    - Tab layout implemented

--- app/student/src/main/java/com/atech/student/ui/faculties/compose/FacultiesScreen.kt
+++ app/student/src/main/java/com/atech/student/ui/faculties/compose/FacultiesScreen.kt
@@ -2,61 +2,142 @@ package com.atech.student.ui.faculties.compose
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.atech.core.retrofit.FacultyModel
 import com.atech.ui_common.R
 import com.atech.ui_common.common.BottomPadding
 import com.atech.ui_common.common.FacultyItem
 import com.atech.ui_common.common.GlobalEmptyScreen
 import com.atech.ui_common.common.MainContainer
+import com.atech.ui_common.theme.ResearchHubTheme
 import com.atech.ui_common.theme.spacing
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FacultiesScreen(
     modifier: Modifier = Modifier, states: List<FacultyModel> = emptyList()
 ) {
     val topAppBarScrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    val tabItems = listOf(
+        "All",
+        "Registered Faculties"
+    )
     MainContainer(
         title = stringResource(id = R.string.faculties),
         modifier = modifier,
         scrollBehavior = topAppBarScrollBehavior
     ) { paddingValues ->
-        if (states.isEmpty()) {
-            GlobalEmptyScreen(
-                modifier = Modifier.padding(paddingValues),
-                title = stringResource(id = R.string.no_faculties_found),
-            )
-            return@MainContainer
-        }
         Column(
             modifier = Modifier
                 .padding(paddingValues)
-                .nestedScroll(topAppBarScrollBehavior.nestedScrollConnection)
-                .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium)
         ) {
-            states.forEach {
-                FacultyItem(
-                    model = it, modifier = Modifier/*.animateItem(
+            var selectedTabIndex by rememberSaveable { mutableIntStateOf(0) }
+            val pagerState = rememberPagerState {
+                tabItems.size
+            }
+            LaunchedEffect(selectedTabIndex) {
+                pagerState.animateScrollToPage(selectedTabIndex)
+            }
+            LaunchedEffect(pagerState.currentPage) {
+                selectedTabIndex = pagerState.currentPage
+            }
+            TabRow(selectedTabIndex = selectedTabIndex) {
+                tabItems.forEachIndexed { index, s ->
+                    Tab(
+                        selected = selectedTabIndex == index,
+                        onClick = {
+                            selectedTabIndex = index
+                        },
+                        text = {
+                            Text(s)
+                        }
+                    )
+                }
+            }
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) { index ->
+                when (index) {
+                    0 -> AllFaculties(states, topAppBarScrollBehavior)
+                    1 -> Box(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("Register")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun AllFaculties(
+    states: List<FacultyModel>,
+    topAppBarScrollBehavior: TopAppBarScrollBehavior
+) {
+    if (states.isEmpty()) {
+        GlobalEmptyScreen(
+            modifier = Modifier,
+            title = stringResource(id = R.string.no_faculties_found),
+        )
+        return
+    }
+    Column(
+        modifier = Modifier
+            .nestedScroll(topAppBarScrollBehavior.nestedScrollConnection)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium)
+    ) {
+        states.forEach {
+            FacultyItem(
+                model = it, modifier = Modifier/*.animateItem(
                         fadeInSpec = spring(
                             dampingRatio = Spring.DampingRatioLowBouncy,
                             stiffness = Spring.StiffnessLow
                         )
                     )*/
-                )
-            }
-            BottomPadding()
+            )
         }
+        BottomPadding()
+    }
+}
+
+@Preview
+@Composable
+private fun FacultyScreenPreview() {
+    ResearchHubTheme {
+        FacultiesScreen()
     }
 }
